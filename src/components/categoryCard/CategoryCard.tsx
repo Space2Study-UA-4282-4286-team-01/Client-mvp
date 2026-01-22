dacf5d561bd5e8a50913c3157a6f2600d0b88189
unknown <zim21den21@gmail.com>
2026-01-22 15:02:52 +0200

    create card category

---
+++ src/components/categoryCard/CategoryCard.tsx
@@ -0,0 +1,41 @@
+import { Typography } from '@mui/material'
+import Box from '@mui/material/Box/Box'
+import { FC } from 'react'
+import { ICONS_MAP } from '~/constants/icons'
+import { CategoryInterface, UserRole, UserRoleEnum } from '~/types'
+import { alpha } from '@mui/material/styles'
+import { styles } from './CategoryCard.styles'
+
+interface CategoryCardProps {
+  category: CategoryInterface
+  role: UserRole | ''
+}
+
+export const CategoryCard: FC<CategoryCardProps> = ({ category, role }) => {
+  const IconComponent =
+    ICONS_MAP[category.appearance.icon.toLowerCase() as keyof typeof ICONS_MAP]
+  const isUserStudent = role === UserRoleEnum.Student
+  const totalOffers = isUserStudent
+    ? `${category.totalOffers.student} ${UserRoleEnum.Student}`
+    : `${category.totalOffers.tutor} ${UserRoleEnum.Tutor}`
+
+  return (
+    <Box sx={styles.cardContainer}>
+      <Box sx={styles.iconContainer}>
+        <Box sx={styles.iconWrapper(alpha(category.appearance.color, 0.12))}>
+          <IconComponent
+            sx={{ color: category.appearance.color, width: 40, height: 40 }}
+          />
+        </Box>
+      </Box>
+      <Box sx={styles.textContainer}>
+        <Typography component='div' sx={styles.categoryName} variant='h6'>
+          {category.name}
+        </Typography>
+        <Typography component='div' sx={styles.total}>
+          {totalOffers}
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
---
+++ src/constants/icons.tsx
@@ -0,0 +1,27 @@
+import {
+  Language,
+  DesignServices,
+  MusicNote,
+  Monitor,
+  Biotech,
+  Tag,
+  Palette,
+  LegendToggle,
+  Star,
+  AccountBalance,
+  Science
+} from '@mui/icons-material'
+
+export const ICONS_MAP = {
+  language: Language,
+  design: DesignServices,
+  music: MusicNote,
+  monitor: Monitor,
+  biotech: Biotech,
+  tag: Tag,
+  palette: Palette,
+  legendToggle: LegendToggle,
+  star: Star,
+  accountBalance: AccountBalance,
+  science: Science
+}
